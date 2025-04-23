6a654450ae34ab0ca82bdaa169ff98a5ce9989d7
Heroid-Dev <www.mhsnbt21@gmail.com>
2025-04-22 22:28:39 -0700

    Change all architecture

--- app/src/main/java/com/example/andoidsample/MainActivity.kt
+++ app/src/main/java/com/example/andoidsample/MainActivity.kt
@@ -4,26 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.andoidsample.ui.theme.AndoidSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,44 +19,22 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndoidSampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyApp(modifier = Modifier.padding(innerPadding)) {
-                        Column {
-                            Text("Text Added")
-                            Text(text = "Hello Android", style = MaterialTheme.typography.titleLarge)
-                        }
-                    }
+                    MyAppDisplay(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun MyApp(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
-    Surface(modifier = modifier.fillMaxSize()) {
-        Box(
-            modifier = Modifier
-                .padding(20.dp)
-                .fillMaxSize()
-                .clip(RoundedCornerShape(20.dp))
-                .background(Color.Blue.copy(.1f)),
-            contentAlignment = Alignment.Center
-        ) {
-            content()
-        }
-    }
-}
+
 
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndoidSampleTheme {
-        MyApp {
-            Column {
-                Text("Text Added")
-                Text(text = "Hello Android", style = MaterialTheme.typography.titleLarge)
-            }
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            MyAppDisplay(modifier = Modifier.padding(innerPadding))
         }
     }
 }

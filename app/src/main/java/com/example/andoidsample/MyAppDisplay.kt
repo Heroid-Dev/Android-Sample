0d4127717a62c25bc8379ff8d03faf0484ca88d4
Heroid-Dev <www.mhsnbt21@gmail.com>
2025-04-22 22:33:31 -0700

    Add welcome text

--- app/src/main/java/com/example/andoidsample/MyAppDisplay.kt
+++ app/src/main/java/com/example/andoidsample/MyAppDisplay.kt
@@ -1,7 +1,9 @@
 package com.example.andoidsample
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,6 +15,9 @@ fun MyAppDisplay(modifier: Modifier = Modifier) {
         modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "Android Hero")
+        Column {
+            Text(text = "Welcome")
+            Text(text = "Android Hero", style = MaterialTheme.typography.headlineSmall)
+        }
     }
 }

9bb3f163e67bf257d718fa4096558d934c6bc560
Heroid-Dev <www.mhsnbt21@gmail.com>
2025-04-22 21:46:23 -0700

    change name of greeting

--- app/src/main/java/com/example/andoidsample/MainActivity.kt
+++ app/src/main/java/com/example/andoidsample/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             AndoidSampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android Hero",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
